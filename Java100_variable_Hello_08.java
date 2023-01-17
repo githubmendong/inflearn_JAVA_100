0ae486148c9069553839f143e80353fdda95e194
shin-dongheon <tlsejdgjs@naver.com>
2023-01-17 20:35:14 +0900

    1017

--- Java100_variable_Hello_08.java
+++ Java100_variable_Hello_08.java
@@ -1,4 +1,4 @@
-public class HelloWorld_test{
+public class Java100_variable_Hello_08{
     public static void main(String[] args){
         System.out.println("Hello World");
     }
---
+++ variable_Helloworld_8.java
@@ -0,0 +1,5 @@
+public class variable_Helloworld_8 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
